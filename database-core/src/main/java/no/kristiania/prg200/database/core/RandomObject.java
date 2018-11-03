548406a4b557f070e098ab6201734a85f433d258
LegoLine <line_ronning93@hotmail.com>
2018-11-03 01:22:23 +0100

    Created a RandomObject-class :rocket:

---
+++ database-core/src/main/java/no/kristiania/prg200/database/core/RandomObject.java
@@ -0,0 +1,8 @@
+package no.kristiania.prg200.database.core;
+
+/**
+ * TODO: Flytte Random-metoder over i denne klassen og få metodene til å funke de stedene metodene blir brukt.
+ */
+
+public class RandomObject {
+}
